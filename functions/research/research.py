9bb788eb8d1991fa59a6d5b71a3d952bedabdbf1
Nobunaga <medvedmark999@gmail.com>
2025-10-07 05:45:14 +0300

    Разработка базового функционала

--- functions/research/research.py
+++ functions/research/research.py
@@ -1,16 +1,22 @@
 """
-Функция: ResearchFunction
+Функция: Исследования
 """
 
 from functions.base_function import BaseFunction
 
+
 class ResearchFunction(BaseFunction):
-    """TODO: Реализация функции"""
+    """Функция исследований"""
 
     def __init__(self, emulator):
         super().__init__(emulator)
         self.name = "ResearchFunction"
 
     def execute(self):
-        """TODO: Логика выполнения"""
+        """
+        TODO: Реализация исследований (ЭТАП 2)
+
+        На ЭТАПЕ 0 это заглушка
+        """
+        print(f"[DEBUG] [{self.emulator_name}] TODO: Логика исследований (ЭТАП 2)")
         pass
